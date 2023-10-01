f42c7e6aadcaa58fdbc7da3741bee2263cf3d72f
MaxPacheco02 <a01552369@tec.mx>
2023-09-30 18:26:34 -0600

    Updates sep 2023

--- Vanttec_USV/main.m
+++ Vanttec_USV/main.m
@@ -1,14 +1,19 @@
 parameters
 
-n = 1001;
+n = 501;
 t_max = 60;
 t = linspace(0, t_max,n);
-x_or = 2.5*sin(t.*(pi/15));
-y_or = 7*cos(t.*(pi/30))-7.5;
-
-ang = atan2(y_or, x_or);
-x = x_or + l.*cos(ang);
-y = y_or + l.*sin(ang);
+% x_or = 2.5*sin(t.*(pi/15));
+% y_or = 7*cos(t.*(pi/30))-7.5;
+% x_or = t.*0;
+% y_or = t.*2;
+x_or = 2.5.*sin(t.*pi/15)+0.5;
+y_or = 7.*cos(t.*pi/30)-7.5;
+% ang = atan2(y_or, x_or);
+% x = x_or + l.*cos(ang);
+% y = y_or + l.*sin(ang);
+x = x_or;
+y = y_or;
 dx = [diff(x)./diff(t(1:2))];
 dy = [diff(y)./diff(t(1:2))];
 dx = [dx,dx(end)];
@@ -30,8 +35,8 @@ ddy = [ddy,ddy(end),ddy(end)];
 tf = t(length(t));
 ts = tf./(n-1);
 
-lambda_x = 2;
-lambda_y = 1;
+lambda_x = 1;
+lambda_y = 3;
 lambda = [lambda_x; lambda_y];
 k = 1.5;
 omega_bar_x = 30;
@@ -46,17 +51,19 @@ gamma_sqrt_y = sqrt(gamma_y);
 gamma_sqrt = [gamma_sqrt_x ; gamma_sqrt_y];
 %% 
 
-sim('control');
+sim('control_clean');
 x_model = squeeze(out.simout.Data(1,1,:));
 y_model = squeeze(out.simout.Data(2,1,:));
-tau = [out.simout1.Data(:,1),out.simout1.Data(:,3)];
+tau_x = squeeze(out.simout1.Data(:,1,:));
+tau_z = squeeze(out.simout1.Data(:,3,:));
 
 subplot(2,1,1)
 plot(y_model, x_model,'b', y, x,'r');
 legend('trajectory', 'desired trajectory');
-title(['lambda: ',num2str(lambda_x),', ',num2str(lambda_y),' | omega bar: '...
-    ,num2str(omega_bar_x),', ',num2str(omega_bar_y), ' | gamma: '...
-    ,num2str(gamma_x),', ',num2str(gamma_y)]);
+% title(['lambda: ',num2str(lambda_x),', ',num2str(lambda_y),' | omega bar: '...
+%     ,num2str(omega_bar_x),', ',num2str(omega_bar_y), ' | gamma: '...
+%     ,num2str(gamma_x),', ',num2str(gamma_y)]);
+title("XY Graph");
 subplot(4,4,9)
 plot(t, x_model, 'b', t, x, 'r'), title('x');
 %legend('x', 'desired x');
@@ -64,10 +71,10 @@ subplot(4,4,13)
 plot(t, y_model, 'b', t, y, 'r'), title('y');
 %legend('y', 'desired y');
 subplot(4,4,10)
-plot(t, x'-x_model), title(['x error, max:',num2str(max(x'-x_model))]);
+plot(t, x'-x_model), title(['x error']);
 subplot(4,4,14)
-plot(t, y'-y_model), title(['y error, max:',num2str(max(y'-y_model))]);
+plot(t, y'-y_model), title(['y error']);
 subplot(4,2,6)
-plot(t, tau(:,1), 'b', t, tau(:,2), 'r'), title('Tau'), legend('x', 'z');
+plot(t, tau_x, 'b', t, tau_z, 'r'), title('Tau'), legend('x', 'z');
 subplot(4,2,8)
 plot(t, atan2(y,x), 'b'), title('Angle');
